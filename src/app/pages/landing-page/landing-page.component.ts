786638db401a32a8c3b45162f3acd93f4bc92dc0
Oleksandra <alexxa.wrk@gmail.com>
2024-10-29 15:35:03 +0200

    created landing page component

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { HomeSectionComponent } from '../../components/home-section/home-section.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [],
+  imports: [HomeSectionComponent],
   templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.css'
+  styleUrl: './landing-page.component.css',
 })
-export class LandingPageComponent {
-
-}
+export class LandingPageComponent {}
