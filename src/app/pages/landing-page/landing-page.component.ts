6bb51796ac771300a056bba3bb502f5eaa4b782c
Oleksandra Kryvenko <alexxa.wrk@gmail.com>
2024-10-31 18:40:24 +0200

    minor code style improvment

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HomeSectionComponent } from '../../components/home-section/home-section.component';
+import { WhoWeAreSectionComponent } from '../../components/who-we-are-section/who-we-are-section.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [HomeSectionComponent],
+  imports: [HomeSectionComponent, WhoWeAreSectionComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css',
 })
