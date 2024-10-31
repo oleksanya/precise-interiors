1c0fec131305b06fbb10e26d2c5913788697398d
Oleksandra Kryvenko <alexxa.wrk@gmail.com>
2024-10-31 18:41:06 +0200

    create intersection directive

---
+++ src/app/directives/intersection-observer.directive.ts
@@ -0,0 +1,44 @@
+import {
+  Directive,
+  ElementRef,
+  OnInit,
+  OnDestroy,
+  output,
+} from '@angular/core';
+
+@Directive({
+  selector: '[appIntersectionObserver]',
+  standalone: true,
+})
+export class IntersectionObserverDirective implements OnInit, OnDestroy {
+  visible = output<boolean>();
+
+  private observer!: IntersectionObserver;
+
+  constructor(private element: ElementRef) {}
+
+  ngOnInit() {
+    this.observer = new IntersectionObserver(
+      entries => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            this.visible.emit(true);
+            this.observer.disconnect();
+          }
+        });
+      },
+      {
+        threshold: [0.3], // Trigger when 30% of the element is visible
+      }
+    );
+
+    this.observer.observe(this.element.nativeElement);
+  }
+
+  ngOnDestroy() {
+    console.log('Observer disconnected');
+    if (this.observer) {
+      this.observer.disconnect();
+    }
+  }
+}
