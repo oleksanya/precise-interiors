108cc0e286c1b064bd52f992793e2bd440353f55
Oleksandra <alexxa.wrk@gmail.com>
2024-11-04 22:08:46 +0200

    add background gradient

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,8 +22,9 @@ module.exports = {
         'multi-step-gradient': 'linear-gradient(270deg, rgba(255, 255, 255, 0.3) 0%, #386FA4 50%, #133C55 100%)',
         'faded-dark-blue': 'linear-gradient(90deg, rgba(19, 60, 85, 0.1) 0%, #FFFFFF 100%)',
         'soft-gradient': 'linear-gradient(0deg, rgba(245, 245, 245, 0.05) 0%, rgba(255, 255, 255, 0.2) 22%, rgba(245, 245, 245, 0.7) 43%, rgba(245, 245, 245, 0.8) 80%, rgba(245, 245, 245, 1) 95%)',
-        'faded-blue': 'linear-gradient(90deg, rgba(19, 60, 85, 0.4), rgba(19, 60, 85, 0))',
-      
+        'faded-blue': 'linear-gradient(90deg, rgba(19,60,85,0.321187850140056) 0%, rgba(19,60,85,0.7637648809523809) 44%, rgba(255,255,255,1) 100%)',
+        'our-expertise-linear': 'linear-gradient(90deg, rgba(19,60,85,0.17833070728291311) 0%, rgba(19,60,85,0.17) 48%, rgba(255,255,255,1) 100%)',
+        'mobile-dark-blue': 'linear-gradient(90deg, rgba(19,60,85,0.6208858543417367) 0%, rgba(255,255,255,1) 100%)',
       },
     },
   },
