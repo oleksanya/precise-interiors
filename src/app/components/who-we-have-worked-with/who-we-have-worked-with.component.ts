9b04c8848ab9fa29a4b1faf299a3bea74e0db3c6
Oleksandra Kryvenko <alexxa.wrk@gmail.com>
2024-11-05 23:54:59 +0200

    create who we ahave worked component

---
+++ src/app/components/who-we-have-worked-with/who-we-have-worked-with.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-who-we-have-worked-with',
+  standalone: true,
+  imports: [],
+  templateUrl: './who-we-have-worked-with.component.html',
+  styleUrl: './who-we-have-worked-with.component.css',
+})
+export class WhoWeHaveWorkedWithComponent {
+  logos = [
+    { imgSrc: 'worked-with-logos/Ace-Logo.png' },
+    { imgSrc: 'worked-with-logos/Opteum.png' },
+  ];
+}
