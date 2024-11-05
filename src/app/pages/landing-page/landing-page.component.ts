2dd91c3cbac48244bafb416d6a92c835d3fd06cb
Oleksandra Kryvenko <alexxa.wrk@gmail.com>
2024-11-05 23:55:08 +0200

    updated main page

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -3,11 +3,12 @@ import { HomeSectionComponent } from '../../components/home-section/home-section
 import { WhoWeAreSectionComponent } from '../../components/who-we-are-section/who-we-are-section.component';
 import { OurExpertiseComponent } from "../../components/our-expertise/our-expertise.component";
 import { SliderComponent } from "../../components/slider/slider.component";
+import { WhoWeHaveWorkedWithComponent } from "../../components/who-we-have-worked-with/who-we-have-worked-with.component";
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [HomeSectionComponent, WhoWeAreSectionComponent, OurExpertiseComponent, SliderComponent],
+  imports: [HomeSectionComponent, WhoWeAreSectionComponent, OurExpertiseComponent, SliderComponent, WhoWeHaveWorkedWithComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css',
 })
