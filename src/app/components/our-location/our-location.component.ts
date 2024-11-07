c73cb04830e4a1c5a3850587206eec11147beb44
Oleksandra Kryvenko <alexxa.wrk@gmail.com>
2024-11-07 18:20:15 +0200

    create our location component

---
+++ src/app/components/our-location/our-location.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-our-location',
+  standalone: true,
+  imports: [],
+  templateUrl: './our-location.component.html',
+  styleUrl: './our-location.component.css',
+})
+export class OurLocationComponent {}
