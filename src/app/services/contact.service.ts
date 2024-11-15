7bbc200bd3f949aa46a1c9d396ad6b816dbf6187
Oleksandra Kryvenko <alexxa.wrk@gmail.com>
2024-11-15 16:57:02 +0200

    update service for sending email with job application form

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -1,14 +1,17 @@
 import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
 import { Injectable } from '@angular/core';
 import { GetInTouchForm } from '../interfaces/get-in-touch-form';
+import { JobApplication } from '../interfaces/job-application';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ContactService {
   private PUBLIC_KEY = 't-tR-zJp4NsNumJ6X';
-  private TEMPLATE_ID = 'template_xblk3bj';
+  private TEMPLATE_ID = 'template_xblk3bj';//contact-form
   private SERVICE_ID = 'service_4be0y32';
+
+  private TEMPLATE_ID_Job = 'template_550ingz';//job-form
 
   sendEmail(templateParams: GetInTouchForm): Promise<EmailJSResponseStatus> {
     return emailjs.send(
@@ -17,5 +20,14 @@ export class ContactService {
       templateParams,
       this.PUBLIC_KEY
     );
+  }
+
+  sendEmailJobApplication(templateParams: JobApplication ): Promise<EmailJSResponseStatus> {
+    return emailjs.send(
+      this.SERVICE_ID,
+      this.TEMPLATE_ID_Job,
+      templateParams,
+      this.PUBLIC_KEY
+    )
   }
 }
