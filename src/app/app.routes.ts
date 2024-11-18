b5ae2014054b77e24dc2573c344b30c6ee5a53c3
Oleksandra Vereshchagina <alexxa.vereshchagina@gmail.com>
2024-11-18 12:51:34 +0200

    init past projects component, add route for this page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { JoinTheTeamPageComponent } from './pages/join-the-team-page/join-the-team-page.component';
+import { PastProjectPageComponent } from './pages/past-project-page/past-project-page.component';
 
 export const routes: Routes = [
   { path: 'home', component: LandingPageComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'join-team', component: JoinTheTeamPageComponent },
+  { path: 'our-projects', component: PastProjectPageComponent },
 ];
---
+++ src/app/components/project-gallery/project-gallery.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-gallery',
+  standalone: true,
+  imports: [],
+  templateUrl: './project-gallery.component.html',
+  styleUrl: './project-gallery.component.css'
+})
+export class ProjectGalleryComponent {
+
+}
