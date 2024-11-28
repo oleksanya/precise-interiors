bb3bf0ce84e7cd24c26b39a596ffa4e04d29c2b3
Oleksandra <alexxa.wrk@gmail.com>
2024-11-28 16:56:49 +0200

    update routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { LandingPageComponent } from './pages/landing-page/landing-page.componen
 import { JoinTheTeamPageComponent } from './pages/join-the-team-page/join-the-team-page.component';
 import { PastProjectPageComponent } from './pages/past-project-page/past-project-page.component';
 import { WorkingProcessPageComponent } from './pages/working-process-page/working-process-page.component';
+import { HealthComponent } from './pages/health/health.component';
 
 export const routes: Routes = [
   { path: 'home', component: LandingPageComponent },
@@ -11,4 +12,5 @@ export const routes: Routes = [
   { path: 'join-team', component: JoinTheTeamPageComponent },
   { path: 'our-projects', component: PastProjectPageComponent },
   { path: 'working-process', component: WorkingProcessPageComponent },
+  { path: 'health', component: HealthComponent }
 ];
