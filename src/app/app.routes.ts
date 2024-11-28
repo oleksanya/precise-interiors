888d2b41d64b75f032d9bdc6f76d082d3bbc316a
Oleksandra Kryvenko <alexxa.wrk@gmail.com>
2024-11-28 16:15:32 +0200

    update route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { JoinTheTeamPageComponent } from './pages/join-the-team-page/join-the-team-page.component';
 import { PastProjectPageComponent } from './pages/past-project-page/past-project-page.component';
+import { WorkingProcessPageComponent } from './pages/working-process-page/working-process-page.component';
 
 export const routes: Routes = [
   { path: 'home', component: LandingPageComponent },
@@ -9,4 +10,5 @@ export const routes: Routes = [
   { path: 'who-we-are', component: LandingPageComponent },
   { path: 'join-team', component: JoinTheTeamPageComponent },
   { path: 'our-projects', component: PastProjectPageComponent },
+  { path: 'working-process', component: WorkingProcessPageComponent },
 ];
